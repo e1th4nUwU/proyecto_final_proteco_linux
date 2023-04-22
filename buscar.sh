e821ee0aeccfa645421f6b4031bec10b6607bea6
Jorge Eithan Treviño Selles <eiteenene18@gmail.com>
2023-04-22 00:36:18 -0600

    [buscar][completado][eithan] Se terminó de crear un programa con el cual pudiéramos buscar si una archivo existe dentro de un folder dado.

---
+++ buscar.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+function search()
+{
+	if [ -d $1 ] ; then
+		if [[ -f "$1/$2" ]]; then
+			echo -e "\n\e[92m	EL ARCHIVO SI ESTA EN EL DIRECTORIO"
+		else 
+			echo -e "\n\e[33m	EL ARCHIVO NO ESTA EN EL ESE DIRECTORIO"
+		fi
+	else
+		echo -e "\n\e[31m	!!! NO EXISTE ESE DIRECTORIO !!!"
+	fi
+}
+if [ $1 ]; then
+	if [ $2 ] ; then
+		search $1 $2
+	else
+		read -p $'\e[35m-> \e[36mIngrese el nombre del archivo que se va a buscar: '  dir
+		search $1 $dir
+	fi
+else 
+	read -p $'\e[35m-> \e[36mIngrese el nombre del directorio dentro del que se va a buscar: '  dir
+	read -p $'\e[35m-> \e[36mIngrese el nombre del archivo a buscar: '  file
+	search $dir $file
+fi
+echo -e "\e[0m"
